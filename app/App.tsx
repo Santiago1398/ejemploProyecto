6536700af6528b869460bb4c21e1413dd37314be
Santiago1398 <santiagoberrioquintero@gmail.com>
2024-12-18 13:17:58 +0100

    desde laptop

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Layout from "./_layout";
 
+//comentario
 
 export default function App() {
     return (
